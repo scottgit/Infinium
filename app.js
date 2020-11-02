110ae8ca88e6fe6f8c3c5646e92a53928617b400
dale.sakamoto <daletsakamoto@gmail.com>
2020-11-02 14:27:44 -0800

    error-handling added to app.js file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const createError = require('http-errors');
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -38,18 +38,24 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error("The requested resource couldn't be found.");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  const isProduction = environment === "production";
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+
+  // res.render('errors');
 });
 
 module.exports = app;
