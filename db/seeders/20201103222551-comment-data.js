d01c482cc313250a20f13309879507a898b2a37b
Scott Smith <scott.sncsmith@gmail.com>
2020-11-04 13:04:20 -0800

    Made faker data for users, stories, comments

--- db/seeders/20201103222551-comment-data.js
+++ db/seeders/20201103222551-comment-data.js
@@ -1,40 +1,33 @@
 'use strict';
+const faker = require('faker');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Comments', [
-      {
-        comment: 'Interesting story you have here',
-        storyId: 3, 
-        userId: 3, 
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        comment: 'I love this story!!',
-        storyId: 3, 
-        userId: 5, 
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        comment: 'This story was kind of boring...',
-        storyId: 4, 
-        userId: 4, 
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        comment: 'I wish you had included images.',
-        storyId: 4, 
-        userId: 1, 
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ], {});
+    const numStories = 50; //match to stories seeder
+    const numUsers = 35; //match total users
+    const comments = [];
+    const numComments = 70;
+
+    function getRandom(max) {
+      return Math.floor(Math.random() * max) + 1;
+    }
+
+    for(let i =0; i<numComments; i++) {
+      const createdAt = faker.date.past(0);
+      const userId = getRandom(numUsers);
+      const storyId = getRandom(numStories);
+      comments.push({
+        comment: `Comment on story '${storyId.toString(16)}' (${storyId}) by user '${userId}' with gibberish ${faker.random.words(getRandom(5) + 2)}`,
+        storyId,
+        userId,
+        createdAt,
+        updatedAt: faker.date.between(createdAt, new Date()),
+      })
+    }
+    return queryInterface.bulkInsert('Comments', comments, {});
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Comments', null, {});
   }
-};
+}
