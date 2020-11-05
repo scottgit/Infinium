257bbb7876119c685b5f52513b78664ffe2d64af
dale.sakamoto <daletsakamoto@gmail.com>
2020-11-04 21:16:57 -0800

    more updates on user page

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -11,6 +11,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
     })
   document.querySelector(".about").addEventListener("click", event => {
     event.preventDefault();
+    let question = document.querySelector(".question");
+    if (question) {
+      document.querySelector('.question_ask').classList.add("hide")
+      document.querySelector('.question_answer').classList.add("hide")
+    }
     document.querySelector('.about').classList.add("hide")
     document.querySelector('.dot').classList.add("hide")
     document.querySelector('.person_info').classList.remove("hide")
