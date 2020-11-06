a8a3e92b3593fd639ea8613c80f46b5fdce32cba
Scott Smith <scott.sncsmith@gmail.com>
2020-11-05 16:18:07 -0800

    Publish view

--- public/javascripts/story-edits.js
+++ public/javascripts/story-edits.js
@@ -1,20 +1,33 @@
 const publishButton = document.querySelector('.story-publish');
 
 if (publishButton) {
+  editorForm = document.getElementById('story-editor');
+  publisherForm = document.getElementById('story-publisher');
+
   publishButton.addEventListener('click', e => {
-    editorForm = document.getElementById('story-editor');
-    publisherForm = document.getElementById('story-publisher');
     //Show the form
     publisherForm.classList.remove('hide');
     //Grab any title updates
     publisherForm.querySelector('.story-publish-title').value = editorForm.querySelector('.story-edit-title').value;
     //Grab any draft updates
-    publisherForm.querySelector('.story-publish-draft').value = editorForm.querySelector('.story-edit-title').value;
+    publisherForm.querySelector('.story-publish-draft').value = editorForm.querySelector('.story-edit-draft').value;
   });
 
   const imageRoute = document.querySelector('.story-publish-image-path');
   const publishImage = document.querySelector('.story-publish-image');
+  if (publishImage.src) publishImage.classList.remove('hide');
+
   imageRoute.addEventListener('blur', e => {
     publishImage.src = e.target.value;
+    if (publishImage.src) {
+      publishImage.classList.remove('hide');
+    } else {
+      publishImage.classList.add('hide');
+    }
   });
+
+  const publisherCloseButton = document.querySelector('.story-publisher-close');
+  publisherCloseButton.addEventListener('click', e => {
+    publisherForm.classList.add('hide');
+  })
 }
